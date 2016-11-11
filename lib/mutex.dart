ad919bf49754ec5c6864aa4384b31a78ec6976c0
Hoylen Sue <hoylen@hoylen.com>
2016-11-11 17:42:40 +1000

    Initial commit.

---
+++ lib/mutex.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, Hoylen Sue. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// Mutual exclusion.
+///
+library mutex;
+
+import 'dart:async';
+
+part 'src/mutex.dart';
+part 'src/read_write_mutex.dart';
